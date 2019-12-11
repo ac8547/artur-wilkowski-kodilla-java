6d8d8e4354b61da9e01343ddacd03660d96e2716
ac8547 <ac8547@gmail.com>
2019-12-11 13:05:12 +0100

    update calc

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,4 +1,24 @@
 package com.kodilla.testing.calculator;
 
+class Kalkulator{
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+
+}
+
 public class Calculator {
+    public static void main(String[] args) {
+
+        Kalkulator kalkulator = new Kalkulator();
+
+        System.out.println("result of add: " + kalkulator.add(5, 5));
+        System.out.println("result of subtract: " + kalkulator.add(10,5));
+
+    }
 }
