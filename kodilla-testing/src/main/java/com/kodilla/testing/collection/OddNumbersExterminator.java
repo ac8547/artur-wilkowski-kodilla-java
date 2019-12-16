878bba7a094abaf82ab5a70eaf8c291ea005d513
ac8547 <ac8547@gmail.com>
2019-12-16 17:54:30 +0100

    update

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,25 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
+
 public class OddNumbersExterminator {
+
+    ArrayList<Integer> evenList;
+
+    public OddNumbersExterminator(){
+        evenList = new ArrayList<Integer>();
+    }
+
+public void exterminate(ArrayList<Integer> numbers){
+    for (Integer listOfNumbers : numbers){
+        Integer evenNumber = listOfNumbers;
+        if (evenNumber % 2 == 0){
+            evenList.add(evenNumber);
+        }
+
+
+    }
+
+    }
+
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,57 @@
 package com.kodilla.testing.collection;
 
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.*;
+import java.util.*;
+
 public class CollectionTestSuite {
+
+
+    @Before
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+
+        ArrayList<Integer> oddList = new ArrayList<Integer>();
+        oddList.add(5);
+        oddList.add(7);
+
+        oddNumbersExterminator.exterminate(oddList);
+        if (oddNumbersExterminator.evenList.isEmpty()){
+            System.out.println("the list is empty");
+            }else
+            System.out.println("not empty");
+    }
+
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+
+        ArrayList<Integer> listOfNumbers = new ArrayList<Integer>();
+        listOfNumbers.add(10);
+        listOfNumbers.add(6);
+        listOfNumbers.add(5);
+        listOfNumbers.add(12);
+
+        oddNumbersExterminator.exterminate(listOfNumbers);
+
+        for (int i=0; i<oddNumbersExterminator.evenList.size(); i++){
+            Assert.assertTrue("false: ", oddNumbersExterminator.evenList.get(i) % 2  == 0);
+
+        }
+    }
+
+
 }
+
